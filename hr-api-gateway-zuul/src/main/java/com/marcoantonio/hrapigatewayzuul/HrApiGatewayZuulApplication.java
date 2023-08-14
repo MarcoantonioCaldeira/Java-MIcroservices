ba0f1454d3faa9245f5a377d3c24b52e8a866328
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-08-14 09:13:28 -0300

    configure api gateway zuul

--- hr-api-gateway-zuul/src/main/java/com/marcoantonio/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/marcoantonio/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -2,7 +2,9 @@ package com.marcoantonio.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
