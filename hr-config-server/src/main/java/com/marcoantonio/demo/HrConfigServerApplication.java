58bda2b26d565193bb342a5ac15d8ea5fbb75953
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-08-26 16:00:35 -0300

    hr-user created

--- hr-config-server/src/main/java/com/marcoantonio/demo/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/marcoantonio/demo/HrConfigServerApplication.java
@@ -1,13 +1,23 @@
 package com.marcoantonio.demo;
 
+import org.gradle.cli.CommandLineConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
-
+public class HrConfigServerApplication implements CommandLineConverter{
+	
+	@Value("${spring.cloud.config.server.git.username}")
+	private String username;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
 
+	//Override
+	public static void main(String... args) throws Exception{
+		System.out.print("USERNAME = ");
+	}
 }
