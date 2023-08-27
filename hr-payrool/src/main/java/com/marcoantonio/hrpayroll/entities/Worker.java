d6e53829e0e0a83ce11da65948af7967e1bcd1da
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-08-26 21:03:04 -0300

    adding archives of the complet project

--- hr-payrool/src/main/java/com/marcoantonio/hrpayroll/entities/Worker.java
+++
@@ -1,5 +0,0 @@
-package com.marcoantonio.hrpayroll.entities;
-
-public class Worker {
-
-}
--- hr-worker/src/main/java/com/marcoantonio/hrworker/repositories/WorkerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.marcoantonio.hrworker.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.marcoantonio.hrworker.entities.Worker;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long>{
-
-}
